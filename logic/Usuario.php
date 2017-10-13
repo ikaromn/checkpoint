9ce05dadf4d4a67d387066e396e78ada6360ea48
Ikaro Pinheiro Lopes <ikaropinheiro6@gmail.com>
2017-10-12 23:44:39 -0300

    Adicionando a classe usuário para fazer todas os métodos que são capazes para um usuário fazer

---
+++ logic/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+    class Usuario
+    {
+        protected $nome;
+        protected $username;
+        protected $nomeempresa;
+        private $password;
+
+        public cadastroUsuario($n, $u, $nE, $p){
+            
+        }
+    }
